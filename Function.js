e480612625daee30174550a4c6d165f9ad47e05d
OrangeInky <86501049+OrangeInky@users.noreply.github.com>
2021-07-06 21:21:30 -0400

    Update Function.js

--- Function.js
+++ Function.js
@@ -44,6 +44,22 @@ function buydoublecookiesPerclick () {
     }
 }
 
+function buyautoBakerOne () {
+    if (gamedatas.Basic.cookies >= gamedatas.autobakers.autoBakerOneCost) {
+        gamedatas.Basic.cookies -= gamedatas.autobakers.autoBakerOneCost;
+        gamedatas.autobakers.autoBakerOne += 1;
+        gamedatas.autobakers.autoBakerOneCost *= 1.1;
+        document.getElementById('cookiesbaked').innerHTML = Math.round(gamedatas.Basic.cookies) + " cookies";
+        document.getElementById('autoBakerOne').innerHTML = gamedatas.autobakers.autoBakerOne;
+        document.getElementById('autoBakerOneCost').innerHTML = " " + Math.round(gamedatas.autobakers.autoBakerOneCost);
+    }
+}
+
+function resetgame () {
+    alert('YOU ARE DELETING YOUR SAVE FILE, ARE YOU SURE YOU WANT TO DO THIS?')
+    localStorage.clear();
+}
+
 var AutoBakerLoop = window.setInterval(function() {
     gamedatas.Basic.cookies += gamedatas.autobakers.autoBakerOne * gamedatas.autobakers.autobakerOneMultiplier;
     document.getElementById('cookiesbaked').innerHTML = Math.round(gamedatas.Basic.cookies) + " cookies";
@@ -64,12 +80,14 @@ function load() {
     if (typeof savegame.Basic.MultiplicativeClick !== "undefined") gamedatas.Basic.MultiplicativeClick = savegame.Basic.MultiplicativeClick;
     if (typeof savegame.Basic.cookiesPerClick !== "undefined") gamedatas.Basic.cookiesPerClick = savegame.Basic.cookiesPerClick;
     if (typeof savegame.Basic.cookies !== "undefined") gamedatas.Basic.cookies = savegame.Basic.cookies;
-    if (typeof savegame.autoBakerOne !== "undefined") gamedatas.autoBakerone = savegame.autoBakerone;
-    if (typeof savegame.autobakerOneMultiplier !== "undefined") gamedatas.autobakerOneMultiplier = savegame.autobakerOneMultiplier;
-    if (typeof savegame.autoBakerOneCost !== "undefined") gamedatas.autoBakerOneCost - savegame.autoBakerOneCost;
+    if (typeof savegame.autobakers.autoBakerOne !== "undefined") gamedatas.autobakers.autoBakerone = savegame.autobakers.autoBakerone;
+    if (typeof savegame.autobakers.autobakerOneMultiplier !== "undefined") gamedatas.autobakers.autobakerOneMultiplier = savegame.autobakers.autobakerOneMultiplier;
+    if (typeof savegame.autobakers.autoBakerOneCost !== "undefined") gamedatas.autobakers.autoBakerOneCost = savegame.autobakers.autoBakerOneCost;
     }
     document.getElementById('cookiesPerClick').innerHTML = "your current cookies per click: " + gamedatas.Basic.cookiesPerClick;
     document.getElementById('cookiesbaked').innerHTML = Math.round(gamedatas.Basic.cookies) + " cookies";
     document.getElementById('doublecookiesPerclickCost').innerHTML = "Upgrade 2 Cost: " + Math.round(gamedatas.Basic.doublecookiesPerclickCost);
     document.getElementById('cookiesPerClickCost').innerHTML = "Upgrade 1 Cost: " + Math.round(gamedatas.Basic.cookiesPerClickCost);
+    document.getElementById('autoBakerOne').innerHTML = gamedatas.autobakers.autoBakerOne;
+    document.getElementById('autoBakerOneCost').innerHTML = " " + Math.round(gamedatas.autobakers.autoBakerOneCost);
 }
